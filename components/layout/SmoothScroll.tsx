54f937f12133b0194fa009ceff4bfe2869d43c30
Cyril John Ypil <cyrilypil@gmail.com>
2026-06-23 23:01:46 +0800

    Fix error on lenis smoothTouch

--- components/layout/SmoothScroll.tsx
+++ components/layout/SmoothScroll.tsx
@@ -5,7 +5,7 @@ import 'lenis/dist/lenis.css'
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.06, duration: 1.2, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.06, duration: 1.2}}>
       {children}
     </ReactLenis>
   )
